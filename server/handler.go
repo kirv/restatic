a5b71e1d8e81609daa189fd0ce251b706641546f
Ken Irving <ken.irving@alaska.edu>
2021-10-13 15:39:25 -0800

    server/handler.go: add thinobject classifier

--- server/handler.go
+++ server/handler.go
@@ -10,6 +10,7 @@ import (
 	"path"
 	"path/filepath"
 	"time"
+    "strings"
 
 	"github.com/relogHQ/restatic/config"
 )
@@ -25,17 +26,21 @@ type fInfo struct {
 	ModTime string
 	Size    string
 	Path    string
-	IsDir   bool
-
-    // thinobject fields follow
-    IsSymlink    bool
-    IsType    bool
-    IsDeclaration    bool
-    IsSymvar    bool
-    Value   string
-    IsParams    bool
-    IsList    bool
-    IsMap    bool
+	IsDir   bool                // ordinary directory, possibly via symlink
+    // thinobject (tob) fields:
+    IsFile          bool        // ordinary file, possibly via symlink
+    IsList          bool        // ordinary file with @ prefix, treat as list of lines
+    IsMap           bool        // ordinary file with % prefix, treat as map or dictionary of key value lines
+    IsSymlink       bool        // symlink, could resolve to a file or directory or not
+    IsDeclaration   bool        // non-resolving symlink, but not a symvar
+    IsType          bool        // non-resolving symlink where name has prefix ^NAME
+    IsSymvar        bool        // non-resolving symlink where value has prefix =
+    IsParameters    bool        // symvar where the name has suffix :
+    Value           string      // symvar value with prefix = removed, or @LIST contents, or %MAP contents
+    IsComponent     bool        // a component is a directory containing ^
+    ComponentType   string      // value of component/^ symlink
+    IsPrototype     bool
+    IsMixin         bool
 }
 type dInfo struct {
 	Name string
@@ -57,40 +62,115 @@ func ByteCountIEC(b int64) string {
 }
 
 func toFInfo(entry os.DirEntry, pwd string) *fInfo {
-	info, err := entry.Info()
+    // each file in the directory is handled here in turn
+	lstat, err := entry.Info()
 	if err != nil {
 		return nil
 	}
+    var tob fInfo
+
+    // entry includes the base path, the file name, and a link to, I think, the output of os.Stat()
+    // Now compare entry.lstat with os.Stat()
+    // entry is the output from os.ReadDir() and 'lstat' is really the output from os.Lstat()!
 
 	path, err := filepath.Rel(config.Directory, pwd)
 	if err != nil {
 		return nil
 	}
-    // fmt.Println("DEBUG1", info.Name(), entry.Name(), path)
+    name := lstat.Name()
+    pathname := path + "/" + name
+
+	stat, err := os.Stat(pathname)
+	if err != nil {
+        tob.IsSymlink = true
+        // The entry is a non-resolving symlink.. if not something more exotic.
+        // It could just be a bad symlink, i.e., one where the target file has been
+        // renamed or removed, but to thinobject a non-resolving non-symvar symlink
+        // is a 'declaration', possibly a 'type' declaration.
+        tob.Value, err = os.Readlink(pathname)
+    	if err != nil {
+    		return nil
+    	}
+        if strings.HasPrefix(tob.Value, "=") {
+            tob.IsSymvar = true
+            // a symvar is a non-resolving symlink where value starts with =
+            tob.Value = strings.TrimPrefix(tob.Value, "=")
+            // fmt.Println("DEBUG SYMVAR:", name, tob.Value)
+            if strings.HasSuffix(name, ":") {
+                tob.IsParameters = true
+                // a symvar with name ending in : is assumed to be a parameters list
+                // fmt.Println("DEBUG PARAMETERS:", name, tob.Value)
+            }
+        } else {
+            // non-resolving non-symvar symlinks are 'types' or 'declarations'
+            if strings.HasPrefix(name, "^") {
+                // non-resolving symlink named ^ or ^foo denotes a thinobject type
+                tob.IsType = true
+                // fmt.Println("DEBUG TYPE:", name, tob.Value)
+            } else {
+                tob.IsDeclaration = true
+                // fmt.Println("DEBUG DECLARATION:", name, tob.Value)
+            }
+        }
+    } else { // must be either a file or a directory, perhaps through a symlink
+        if stat.IsDir() {
+            tob.IsDir = true
+            // name might be a local directory or might be a symlinked one
+            // fmt.Println("DEBUG DIRECTORY:", name)
+	        isComponent, _ := os.Lstat(pathname + "/^")
+            if isComponent != nil {
+                tob.IsComponent = true
+                // a directory containing ^ is a component of this object
+                tob.ComponentType, err = os.Readlink(pathname + "/^")
+            	if err != nil {
+            		return nil
+            	}
+                // fmt.Println("DEBUG COMPONENT:", name, "TYPE:", tob.ComponentType)
+            }
+        } else {
+            tob.IsFile = true
+            // name might be a local or symlinked file
+            // fmt.Println("DEBUG FILE:", name)
+            if strings.HasPrefix(name, "@") {
+                tob.IsList = true
+                // fmt.Println("DEBUG LIST:", name)
+            } else if strings.HasPrefix(name, "%") {
+                tob.IsMap = true
+                // fmt.Println("DEBUG MAP:", name)
+            }
+        }
+    }
+
+    if tob.IsList || tob.IsMap {
+    	b, err := os.ReadFile(pathname)
+    	if err != nil {
+    		return nil
+    	}
+        tob.Value = string(b)
+    }
 
 	return &fInfo{
 		Name:           entry.Name(),
-		Mode:           info.Mode().String(),
-		ModTime:        info.ModTime().Format(time.RFC1123),
-		Size:           ByteCountIEC(info.Size()),
+		Mode:           lstat.Mode().String(),
+		ModTime:        lstat.ModTime().Format(time.RFC1123),
+		Size:           ByteCountIEC(lstat.Size()),
 		Path:           path,
 		IsDir:          entry.Type().IsDir(),
-        IsSymlink:      false,
-        IsType:         false,
-        IsDeclaration:  false,
-        IsSymvar:       false,
-        Value:          "no value",
-        IsParams:       false,
-        IsList:         false,
-        IsMap:          false,
+        IsFile:         tob.IsFile,
+        IsList:         tob.IsList,
+        IsMap:          tob.IsMap,
+        IsSymlink:      tob.IsSymlink,
+        Value:          tob.Value,
+        IsType:         tob.IsType,
+        IsDeclaration:  tob.IsDeclaration,
+        IsSymvar:       tob.IsSymvar,
+        IsParameters:   tob.IsParameters,
 	}
-
 }
 
 func toFInfos(infos []os.DirEntry, pwd string) []*fInfo {
 	fInfos := make([]*fInfo, len(infos))
 	for i, info := range infos {
-        // fmt.Println("DEBUG6 in toFInfos()", i, info.Name())
 		fInfos[i] = toFInfo(info, pwd)
 	}
 	return fInfos
@@ -136,14 +216,11 @@ func writeDirectory(w http.ResponseWriter, path string, dirInfo os.FileInfo) {
 }
 
 func writeFile(w http.ResponseWriter, path string, info os.FileInfo) {
-    // fmt.Println("DEBUG7 writeFile() try os.Open()", info.Name())
 	f, err := os.Open(path)
 	if os.IsNotExist(err) {
-        // fmt.Println("DEBUG8 writeFile() failed", info.Name())
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
-    // fmt.Println("DEBUG9 writeFile()", info.Name())
 	if err != nil {
 		write500(w)
 		return
@@ -168,19 +245,8 @@ func (f fsHandler) ServeHTTP(w http.ResponseWriter, request *http.Request) {
 
 	cPath := path.Clean(path.Join(baseDir, request.URL.Path))
 
-    // fmt.Println("DEBUG10 ServeHTTP() baseDir:", baseDir, "r.URL.Path:", request.URL.Path)
-
-	// linfo, err := os.Lstat(cPath)
-	// if os.IsNotExist(err) {
-	// 	http.NotFound(w, request)
-	// 	return
-	// }
-
 	info, err := os.Stat(cPath)
-    // fmt.Println("DEBUG11 info:", info)
-    // fmt.Println("DEBUG12 err:", err)
 	if os.IsNotExist(err) {
-        // fmt.Println("DEBUG13 os.IsNotExist() called")
 		http.NotFound(w, request)
 		return
 	}
@@ -195,19 +261,16 @@ func (f fsHandler) ServeHTTP(w http.ResponseWriter, request *http.Request) {
 		iPath := path.Clean(path.Join(cPath, "index.html"))
 		iInfo, err := os.Stat(iPath)
 		if err == nil && !iInfo.IsDir() {
-            // fmt.Println("DEBUG2 call writeFile() with", iPath)
             // HINT: this point is reached if index.html is found
 			writeFile(w, iPath, iInfo)
 			return
 		} else {
-            // fmt.Println("DEBUG3 call writeDirectory() with", cPath)
             // HINT: this point is reached when a directory without index.html is entered
 			writeDirectory(w, cPath, info)
 		}
 		return
 	}
 
-    // fmt.Println("DEBUG4 call writeFile() with", cPath)
     // HINT: render contents of this file to the webpage
 	writeFile(w, cPath, info)
 }
